04091974600d8280e5b4eab3c0e258f879772f93
rehagursel <92670670+rehagursel@users.noreply.github.com>
2022-04-12 20:52:23 +0300

    add Product components and code updates

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,8 +1,45 @@
-import Card from '../UI/Card';
-import classes from './ProductItem.module.css';
+import { useDispatch, useSelector } from "react-redux";
+
+import { cartActions } from "../../store/cart-slice";
+import Card from "../UI/Card";
+import classes from "./ProductItem.module.css";
 
 const ProductItem = (props) => {
-  const { title, price, description } = props;
+  const cart = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
+  const { title, price, description, id } = props;
+
+  const addToCartHandler = () => {
+    const newTotalQuantity = cart.totalQuantity + 1;
+
+    const currentItems = cart.items.slice(); // create copy via slice to avoid mutating original state
+    const existingItem = currentItems.find((item) => item.id === id);
+    if (existingItem) {
+      const updatedItem = { ...existingItem }; // new object + copy existing properties to avoid state mutation
+      updatedItem.quantity++;
+      updatedItem.totalPrice +=  price;
+      const existingItemIndex = currentItems.findIndex(
+        (item) => item.id === id
+      );
+      currentItems[existingItemIndex] = updatedItem;
+    } else {
+      currentItems.push({
+        id: id,
+        price: price,
+        quantity: 1,
+        totalPrice: price,
+        name: title,
+      });
+    }
+
+    const newCart = {
+      totalQuantity: newTotalQuantity,
+      items: currentItems,
+    };
+
+    dispatch(cartActions.replaceCart(newCart));
+  };
 
   return (
     <li className={classes.item}>
@@ -13,7 +50,7 @@ const ProductItem = (props) => {
         </header>
         <p>{description}</p>
         <div className={classes.actions}>
-          <button>Add to Cart</button>
+          <button onClick={addToCartHandler}>Add to Cart</button>
         </div>
       </Card>
     </li>
--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,41 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const dummyProducts = [
+  {
+    id: "p1",
+    price: 6,
+    title: "Sandwich",
+    description: "With beef and salad",
+  },
+  {
+    id: "p2",
+    price: 10,
+    title: "Fettuccine Alfredo",
+    description: "With cream cheese",
+  },
+  {
+    id: "p3",
+    price: 15,
+    title: "Beef with vegetables",
+    description: "With souce",
+  },
+];
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {dummyProducts.map((product) => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
